d9a8649a45b8a28b4742116e4415c848530671c1
Dang Quang <quangthat97@gmail.com>
2023-11-11 00:14:59 +0700

    commit mau

---
+++ src/components/sidebar/links/Link.jsx
@@ -0,0 +1,46 @@
+import { motion } from 'framer-motion';
+
+const variants = {
+	open: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+	closed: {
+		transition: {
+			staggerChildren: 0.05,
+			staggerDireaction: -1,
+		},
+	},
+};
+
+const itemVariants = {
+	open: {
+		y: 0,
+		opacity: 1,
+	},
+	closed: {
+		y: 50,
+		opacity: 0,
+	},
+};
+
+export default function Link() {
+	const items = ['Home', 'Services', 'Portfolio', 'Contact', 'About'];
+
+	return (
+		<motion.div className='links' variants={variants}>
+			{items.map((item) => (
+				<motion.a
+					href={`#${item}`}
+					key={item}
+					variants={itemVariants}
+					whileHover={{ scale: 1.1 }}
+					whileTap={{ scale: 0.5 }}
+				>
+					{item}
+				</motion.a>
+			))}
+		</motion.div>
+	);
+}
